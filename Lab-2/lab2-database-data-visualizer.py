51145647041429c0d2dbf1d9eb49fcf397e6ae43
Mohamed Selim <97752834+mselim6022@users.noreply.github.com>
2022-01-23 18:01:46 -0500

    Create lab2-database-data-visualizer.py

---
+++ Lab-2/lab2-database-data-visualizer.py
@@ -0,0 +1,36 @@
+from pandas import read_sql_query
+import sqlite3
+import plotly.graph_objects as go
+import plotly.express as px
+
+from plotly.subplots import make_subplots
+
+#Connect the database
+dbconnect = sqlite3.connect("sensorDB.db")
+
+#Read the database into a DataFrame
+df = read_sql_query('''SELECT * from sensordata''', dbconnect, "id", "readTime")
+
+#Make a plot with a secondary y axis. Since the values of temperature and humidity are around the same
+#order of magnitude, we will plot them on the same y axis. The values of pressure will be on a separate
+#y axis.
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+
+#Add trace for temperature and humidity values
+fig.add_trace(
+	go.Scatter(x=df['readTime'], y= df['temperature'], name = "Temperature(C)", mode="markers"),
+	secondary_y = False
+)
+fig.add_trace(
+	go.Scatter(x=df['readTime'], y=df['humidity'], name = "Humidity(%)", mode="markers"),
+	secondary_y = False
+)
+
+#Add trace for pressure
+fig.add_trace(
+	go.Scatter(x = df['readTime'], y = df['pressure'], name = "Pressure(millibars)", mode= "markers"),
+	secondary_y = True
+)
+
+fig.write_html('tmp.html', auto_open = True)
