4e21acbee3b7185b6e1ff94de416c1b1a7db9a95
Mohamed Selim <97752834+mselim6022@users.noreply.github.com>
2022-02-18 12:17:35 -0500

    Update main_rpi.py
    First attempt

--- Lab-5/mini-project/main_rpi.py
+++ Lab-5/mini-project/main_rpi.py
@@ -1,36 +1,60 @@
 from helper_functions import camera, computer_vision,sensehat
-### TO-DO: You may require more imports
+from time import sleep
 
 def main():
-    camera_i = camera.get_camera() #DO NOT MODIFY, function call must work as is 
-    sense = sensehat.get_sensehat() #DO NOT MODIFY, function call must work as is 
-    
-    take_background_image = True #TO-DO: Should be a user input
-
-    if take_background_image:
-        ### TO-DO: Countdown image capture of background  
-        preview = False
-        countdown=0
-        camera.capture_image(camera_i,"data/images/background.jpg", countdown_time=countdown, preview=preview) #DO NOT MODIFY, function call must work as is 
-    
-    arm_system = True #TO-DO: Should be a user input
-
-    if arm_system:
-        interval = 10 #TO-DO: Should be a user input
-        t1 = 480000000 #TO-DO: Should be a user input
-       
-       ### TO-DO: Countdown to monitoring
-
-        count = 0
-        while True: #DO NOT MODIFY, function call must work as is 
-            camera.capture_image(camera_i,"data/images/image%s.jpg" % count, countdown_time=interval) #DO NOT MODIFY, function call must work as is 
-            person_detected = computer_vision.person_detected("data/images/background.jpg","data/images/image%s.jpg" % count, t1)  #DO NOT MODIFY, function call must work as is 
-            if person_detected: #DO NOT MODIFY, function call must work as is 
-                print("Person Detected") #DO NOT MODIFY, function call must work as is 
-                sensehat.alarm(sense,interval)  #DO NOT MODIFY, function call must work as is 
+    camera_i = camera.get_camera()
+    sense = sensehat.get_sensehat()
+    while True:
+        option = input("Enter \'1\' if a background image is saved in data/images/background.png\n\
+        Enter \'2\' to take the background image")
+        if(int(option) == 1):
+            take_background_image = False
+        elif(int(option) == 2):
+            take_background_image = True
+        else:
+            continue
+        if (take_background_image):
+            print("Get out of the scene!\n\
+            Background image will be taken in 10 seconds...")
+            for i in range(10):
+                sleep(1)
+                print(10-i)
+                
+            preview = False
+            countdown=0
+            camera.capture_image(camera_i,"data/images/background.jpg", countdown_time=countdown, preview=preview)
+            print("Background image taken!\n")
+            take_background_image = False
+        while (! take_background_image):
+            optionArm = ("Would you like to arm the system? y/n")
+            if (optionArm is "y"):
+                arm_system = True
+            elif(optionArm is "n"):
+                arm_system = False
             else:
-                print("No Person Detected") #DO NOT MODIFY, function call must work as is 
-            count += 1
+                continue
+                
+            if (arm_system):
+                interval = int(input("Enter the interval between test images in seconds:"))
+                t1 = int(input("Enter the threshold t1:"))
+                
+                print("Monitoring will begin in %d seconds..." % interval)
+                for i in range(interval):
+                    sleep(1)
+                    print(interval - i)
+                
+                print("Monitoring has begun!")
+                
+                count = 0
+                while True:
+                    camera.capture_image(camera_i,"data/images/image%s.jpg" % count, countdown_time=interval)
+                    person_detected = computer_vision.person_detected("data/images/background.jpg","data/images/image%s.jpg" % count, t1) 
+                    if person_detected:
+                        print("Person Detected") 
+                        sensehat.alarm(sense,interval) 
+                    else:
+                        print("No Person Detected") 
+                    count += 1
 
 
 if __name__ == "__main__":
