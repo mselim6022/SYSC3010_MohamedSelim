ad12ae91994ec38acf0062a8763527fed786b5e2
Mohamed Selim <97752834+mselim6022@users.noreply.github.com>
2022-02-18 11:16:57 -0500

    Update sensehat.py
    Created functions get_sensehat() and alarm()

--- Lab-5/mini-project/helper_functions/sensehat.py
+++ Lab-5/mini-project/helper_functions/sensehat.py
@@ -1,1 +1,17 @@
+from sense_hat import SenseHat
+from time import sleep
+#This function creates and returns an instance of a SenseHat
+def get_sensehat():
+  sense = SenseHat()
+  return sense
+
+
+def alarm(sense, flash_time):
+  for i in range (flash_time):
+    if (i%2 == 1):
+      sense.clear()
+    else:
+      sense.clear(255,0,0)
+    sleep(1)
+  sense.clear()
 
